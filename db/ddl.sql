c6262fbd2d12b122517e496b3a3c59fc2dd70780
gmiak <kayss.g@gmail.com>
2023-01-14 23:21:30 +0100

    Minor changes before push

--- db/ddl.sql
+++ db/ddl.sql
@@ -2,7 +2,7 @@ INSERT INTO users (username, password, balance, permission, active, joined)
 VALUES
 ('Oscar', '$2a$10$kYVtijnpy/pW5ddtA2.cg.NV9q2WbmloB5PFH2X7tVDT3D7RAvl5m', 100, TRUE, FALSE, '14 jan 2023'),
 ('Mo', '$2a$10$kYVtijnpy/pW5ddtA2.cg.NV9q2WbmloB5PFH2X7tVDT3D7RAvl5m', 200, TRUE, FALSE, '14 jan 2023'),
-('Gorge', '$2a$10$kYVtijnpy/pW5ddtA2.cg.NV9q2WbmloB5PFH2X7tVDT3D7RAvl5m', 10, FALSE, FALSE, '14 jan 2023');
+('Georges', '$2a$10$kYVtijnpy/pW5ddtA2.cg.NV9q2WbmloB5PFH2X7tVDT3D7RAvl5m', 10, FALSE, FALSE, '14 jan 2023');
 -- Password is 'password' encrypted with bcrypt
 
 INSERT INTO scooters (battery, position, live, pickup, active, service, zone, lastUser)
